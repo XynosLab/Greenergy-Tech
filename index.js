3d3e3dd8a6abc07fd61b00f2fcf67c7796030d45
Mahmudur Rahman Labib <admin@mahmudurrahmanlabib.com>
2023-12-11 23:58:35 +0600

    Reup

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ form.addEventListener('submit', (event)=>{
     event.preventDefault();
     var n= document.getElementById('name').value;
     var e= document.getElementById('email').value;
-    var s= document.getElementById('subject').value;
+    var p= document.getElementById('phone').value;
+    var s= document.getElementById('address').value
     var m= document.getElementById('message').value;
 
 
-    var body= "Name: "+n+ "<br>"+ "Email: "+e+ "<br>"+ "Subject: "+s+ "<br>"+ "Message: "+m;
+    var body= "Name: "+n+ "<br>"+ "Email: "+e+ "<br>"+ "Phone: "+p+ "<br>"+  "Address: "+s+ "<br>"+ "Message: "+m;
     //document.write(body)
     Email.send({
         SecureToken : "2201b07b-157a-4d05-b75f-51c9d4fcb8c5",
